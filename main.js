9dfe6ce5cad6e6eab26251dfc99a72ef02d80c57
Unknown <michael.yc.chong@gmail.com>
2017-10-14 21:58:35 -0400

    not sure why this isnt working

--- main.js
+++ main.js
@@ -8,20 +8,26 @@ function myFunction() {
   var searchArg = $("#searchbar").val();
 
   // Deal with it
-  d3.json("https://iscisibs.github.io/fullData.json", function(data) {
+  d3.json("https://iscisibs.github.io/fullData.json", function(data){
+
+    // Record JSON elements with search argument as the "name"
     var bigSibs = data.filter(function(d) {
      return d.name == searchArg;
    });
+
+   // Record JSON elements with search argument as the "bigsib"
     var littleSibs = data.filter(function(d) {
       return d.bigsib == searchArg;
     });
 
+    //Output the big sib data
      d3.select("#bigSibList")
         .selectAll("li")
         .data(bigSibs)
         .enter()
         .append("li").text(function(d) {return d.bigsib});
 
+    // Output the little sib data
       d3.select("#littleSibList")
          .selectAll("li")
          .data(littleSibs)
