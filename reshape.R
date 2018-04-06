3cdd2519a0ba6e5a5422b29bc7c2304efa2639c4
Michael Chong <michael.yc.chong@gmail.com>
2018-04-06 17:00:15 -0400

    deal with removals

--- reshape.R
+++ reshape.R
@@ -46,6 +46,26 @@ for (i in seq_along(fileNames)) {
   fullData <- rbind(fullData, df) 
 }
 
+# Corrections: removals
+correctionRem <- read_csv("removals.csv") %>%
+  mutate(namekey = str_to_lower(name)) %>%
+  mutate(namekey = str_replace_all(namekey, "[[:punct:]]", "")) %>% # Remove punctuation
+  mutate(namekey = str_replace_all(namekey, " ", "")) %>%
+  
+  # Construct keys for bigsibs
+  mutate(bigsibkey = str_to_lower(bigsib)) %>%
+  mutate(bigsibkey = str_replace_all(bigsibkey, "[[:punct:]]", "")) %>% # Remove punctuation
+  mutate(bigsibkey = str_replace_all(bigsibkey, " ", "")) %>%
+  
+  # create an id to remove
+  mutate(removalkey = str_c(namekey, bigsibkey))
+
+fullData <- fullData %>%
+  mutate(relkey = str_c(namekey, bigsibkey)) %>%
+  filter(!relkey %in% correctionRem$removalkey) %>% 
+  select(-relkey)
+  
+# Corrections: additions
 correctionAdd <- read_csv("additions.csv") %>%
   mutate(namekey = str_to_lower(name)) %>%
   mutate(namekey = str_replace_all(namekey, "[[:punct:]]", "")) %>% # Remove punctuation
@@ -56,7 +76,7 @@ correctionAdd <- read_csv("additions.csv") %>%
   mutate(bigsibkey = str_replace_all(bigsibkey, "[[:punct:]]", "")) %>% # Remove punctuation
   mutate(bigsibkey = str_replace_all(bigsibkey, " ", "")) 
 
-fullData <- rbind(fullData, correctionAdd) %>%
+fullData <- bind_rows(fullData, correctionAdd) %>%
   filter((!duplicated(name, fromLast = T) & bigsibkey == "nana") | bigsibkey != "nana")
 
 # Write a .json
@@ -64,8 +84,11 @@ write_json(fullData, "fullData.json")
 
 # Make names for autocomplete
 nameList <- fullData %>%
+  arrange(desc(-year)) %>%
+  group_by(year) %>%
+  arrange(-desc(namekey), .by_group = TRUE) %>%
   filter(!duplicated(name)) %>%
-  mutate(id = name, text = name) %>% 
+  mutate(id = name, text = str_c(name, " (", year, ")")) %>% 
   select(id, text)
 
 write_json(nameList, "nameList.json")
